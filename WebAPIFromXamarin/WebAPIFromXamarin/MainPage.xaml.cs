6d66876e88af20f965e7ba0b3f42f58c1953773a
Abhishek <Abhishek@LAPTOP-BU0QNB5F>
2019-07-30 18:54:00 +0530

    Add project files.

---
+++ WebAPIFromXamarin/WebAPIFromXamarin/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIFromXamarin.Models;
+using Xamarin.Forms;
+
+namespace WebAPIFromXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            try
+            {
+                var todo = GetAPIDataFromWeb();
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+        }
+
+        private async Task<List<Todo>> GetAPIDataFromWeb()
+        {
+            List<Todo> todos = new List<Todo>();
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/todos/1");
+            if (response != null)
+            {
+                todos = JsonConvert.DeserializeObject<List<Todo>>(response);
+                todoListView.ItemsSource = todos;
+            }
+            return todos;
+        }
+    }
+}
---
+++ WebAPIFromXamarin/WebAPIFromXamarin/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAPIFromXamarin.Models
+{
+    public class Todo
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+}
